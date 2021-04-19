76fa095d421673cbd90e14a10844ba4efd28222d
Kim Yong Seok <shikgom2@naver.com>
2021-04-19 18:00:02 +0900

    Update 20500.cpp

--- 20500.cpp
+++ 20500.cpp
@@ -1,27 +1,65 @@
 #include <cstdio>
 #include <algorithm>
 #include <queue>
+#include <cstring>
 #include <iostream>
 using namespace std;
 
-int dp[1516][3];
+int dp[61][61][61]; 
+	
+int solve(int a, int b, int c)
+{
+	if(a < 0)
+	{
+		a = 0;
+	}
+	if(b < 0)
+	{
+		b = 0;
+	} 
+	if(c < 0)
+	{
+		c = 0;
+	} 
+	
+	if(a == 0 && b == 0 && c == 0)
+	{
+		return 0;
+	}
+	
+	int& tmp = dp[a][b][c];
+	if(tmp != -1)
+	{
+		return tmp;
+	}
+	
+	tmp = 999999999;
+	
+	tmp = min(tmp, solve(a-9, b-3, c-1) + 1);
+	tmp = min(tmp, solve(a-9, b-1, c-3) + 1);
+	tmp = min(tmp, solve(a-3, b-1, c-9) + 1);
+	tmp = min(tmp, solve(a-3, b-9, c-1) + 1);
+	tmp = min(tmp, solve(a-1, b-3, c-9) + 1);
+	tmp = min(tmp, solve(a-1, b-9, c-3) + 1);	
+	
+	return tmp;
+}
 
 int main() {
+	
 	int n;
-	int mod = 1000000007;
+	int val[3] = {0,0,0};
+	
 	cin >> n;
 	
-	dp[2][0] = 1;
-	dp[2][1] = 1;
-	dp[2][2] = 0;
-	
-	for(int i = 3; i<= n; i++)
+	for(int i = 0; i<n; ++i	)
 	{
-		dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % mod;
-		dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % mod;
-		dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % mod;
-	}	
-
-	cout << dp[n][0];
+		cin >> val[i];
+	}
+	
+	memset(dp, -1, sizeof(dp));
+	
+	cout << solve(val[0], val[1], val[2]);
+	
     return 0;
 }
