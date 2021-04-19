70ee6c67b5769fb15bb4c462099d4caab2ea0c4a
Kim Yong Seok <shikgom2@naver.com>
2021-04-19 17:59:53 +0900

    Create 16953.cpp

---
+++ 16953.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	queue<pair<long long, int>> q;
+	
+	long long a;
+	long long b;
+	
+	
+	cin >> a >> b;
+	
+	q.push({a,0});
+	
+	while(q.empty())
+	{
+		pair<long long, int> tmp = q.front();
+		cout << tmp.first << endl;
+		q.pop();
+		
+		if(tmp.first == b)
+		{
+			cout << tmp.second + 1 << endl;
+			return 0;
+		}
+		
+		if(tmp.first * 10 + 1 < b)
+		{
+			q.push({tmp.first * 10 + 1 , tmp.second + 1});
+		}
+		if(tmp.first * 2 < b)
+		{
+			q.push({tmp.first * 2, tmp.second + 1});
+		}
+	}
+	
+	cout << "-1" << endl;
+	
+    return 0;
+}
