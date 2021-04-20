d886caf736d5756cfddc23116046ce50d0dec6cf
Kim Yong Seok <shikgom2@naver.com>
2021-04-20 09:27:57 +0900

    Create 10950.cpp

---
+++ 10950.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int i;
+
+    cin >> i;
+
+    while(i--){
+        int a,b;
+        
+        cin >> a >> b;
+
+        cout << a+b << endl;
+    }
+}
