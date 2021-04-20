fbee4149ac6b0ea65dda5eaaae881e929c81965f
Kim Yong Seok <shikgom2@naver.com>
2021-04-20 10:41:19 +0900

    Create 2675.cpp

---
+++ 2675.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int i;
+    cin >> i;
+    char str[20];
+
+    while(i--){
+        
+        for(int a = 0; a<20; a++)
+        {
+            str[a] = "";
+        }
+
+        cin >> str;
+
+    }
+}
