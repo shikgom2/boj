ebdbc76535be9d3e89a5d397d9755d3c3ee7f970
Kim Yong Seok <shikgom2@naver.com>
2021-04-20 09:42:08 +0900

    Create 11654.cpp

---
+++ 11654.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    char i;
+
+    cin >> i;
+    
+    printf("%d", i);
+}
