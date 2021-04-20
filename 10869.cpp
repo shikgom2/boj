40f170efa1de152466f5e479dd835895eb69299e
Kim Yong Seok <shikgom2@naver.com>
2021-04-20 09:31:18 +0900

    Create 10869.cpp

---
+++ 10869.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int i,j;
+
+    cin >> i >> j;
+
+    cout << i+j << endl;
+    cout << i-j << endl;
+    cout << i*j << endl;
+    cout << i/j << endl;
+    cout << i%j << endl;
+}
