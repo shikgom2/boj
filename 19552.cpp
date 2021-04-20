dbb61198e355a0ed3d2b61d4bb331e9cb3fb6c4f
Kim Yong Seok <shikgom2@naver.com>
2021-04-20 09:28:55 +0900

    Create 19552.cpp

---
+++ 19552.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int i;
+
+    while(1){
+        int a,b;
+        
+        cin >> a >> b;
+
+        if(a == 0 && b == 0)
+        {
+            return 0;
+        }
+        cout << a+b << endl;
+    }
+}
