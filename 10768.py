61e3a64af5a08fefc870ca781146aebb2fbe659e
Kim Young Seok <shikgom2@naver.com>
2024-03-20 14:39:29 +0900

    update solved

---
+++ 10768.py
@@ -0,0 +1,12 @@
+i=int(input())
+j=int(input())
+if(i==1):
+    print("Before")
+elif(i>2):
+    print("After")
+elif(j==18):
+    print("Special")
+elif(j>18):
+    print("After")
+else:
+    print("Before")
