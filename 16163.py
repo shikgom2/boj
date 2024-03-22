6d4c814196fb4b6d4a2fc07756327a3f46f993cd
Kim Young Seok <shikgom2@naver.com>
2024-03-23 03:08:31 +0900

    update solved

--- 16163.py
+++ 16163.py
@@ -1,4 +1,3 @@
-result = []
 def manacher(s):
     A = '@#' + '#'.join(s) + '#$'
     Z = [0] * len(A)
@@ -9,24 +8,10 @@ def manacher(s):
             Z[i] = min(right - i, Z[2 * center - i])
         while A[i + Z[i] + 1] == A[i - Z[i] - 1]:
             Z[i] += 1
-            start = (i - Z[i]) // 2
-            end = start + Z[i]
-            if Z[i] > 0:
-                result.append(s[start:end])
         if i + Z[i] > right:
             center, right = i, i + Z[i]
 
-    maxLen, centerIndex = max((n, i) for i, n in enumerate(Z))
-    start = (centerIndex - maxLen) // 2
-    return s[start:start + maxLen]
+    return sum((z + 1) // 2 for z in Z)
 
 s = input()
-manacher(s)
-
-cnt = 0
-for res in result:
-    if(len(res) > 1 and len(res) % 2 == 1):
-        print(res)
-        cnt += 1
-
-print(cnt + len(s))
+print(manacher(s))
