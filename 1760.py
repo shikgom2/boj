ba41099ffd41b44d1743e14e86e72c07d4d38ab2
Kim Young Seok <shikgom2@naver.com>
2024-04-30 18:55:35 +0900

    update solved
    update solved

--- 1760.py
+++ 1760.py
@@ -12,39 +12,60 @@ def dfs(x):
             return True
     return False
 
-V = 101
+V = 10001
 graph = [[] for _ in range(V+1)]
 d = [0] * (V+1)
 
 n, m = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(n)]
 
-
-
-
-board = [[] for _ in range(n)]
-idx = 1
-for i in range(n):
-    li = list(map(int, input().split()))
-    for j in range(len(li)):
-        if(li[j] == 0):
-            board[i].append(idx)
-        elif(li[j] == 1):
-            board[i].append(0)
-        elif(li[j] == 2):
-            idx += 1
-            board[i].append(0)
-print(board)
-
-
+# 행 영역 별로 번호를 매긴다.
+raw = [[0] * m for _ in range(n)]; ri = 0
+is_open = False # 행이나 열이 이어지고 있는지 판별
 for i in range(n):
     for j in range(m):
-        if(board[i][j] != board[i][j]):
-            
+        if matrix[i][j] == 2:
+            if is_open:
+                is_open = False
+                ri += 1
+        else:
+            if not is_open:
+                is_open = True
+            raw[i][j] = ri
+    if is_open:
+        is_open = False
+        ri += 1
+#print(raw)
+
+# 열 영역 별로 번호를 매긴다.
+col = [[0] * m for _ in range(n)]; ci = 0
+for j in range(m):
+    for i in range(n):
+        if matrix[i][j] == 2:
+            if is_open:
+                is_open = False
+                ci += 1
+        else:
+            if not is_open:
+                is_open = True
+            col[i][j] = ci
+    if is_open:
+        is_open = False
+        ci += 1
+
+#print(col)
+
+# 각 격자마다 빈 격자라면 행 영역에서 열 영역으로 이어지는 간선을 그래프에 추가
+graph = [[] for _ in range(V+1)]
+for i in range(n):
+    for j in range(m):
+        if not matrix[i][j]:
+            graph[raw[i][j]+1].append(col[i][j]+1)
 
 #print(graph)
 
 ans = 0
-for i in range(1, m + 1):
+for i in range(1, V + 1):
     c = [False] * (V+1)
     if dfs(i):
         ans += 1
